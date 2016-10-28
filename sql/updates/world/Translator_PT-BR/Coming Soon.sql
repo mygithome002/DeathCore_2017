3aab46f5f034e1b7b66e8071ae7281630cc443bf
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-28 09:36:36 -0300

    Quick Fix

--- sql/updates/world/Translator_PT-BR/Coming Soon.sql
+++
@@ -1,7 +0,0 @@
--- Working for Access Requirement
--- By Gabriel Ferreira for FineWoWCore.
-
-UPDATE `world`.`access_requirement` SET `level_max`=85 WHERE  `mapId`=939 AND `difficulty`=1;
-UPDATE `world`.`access_requirement` SET `level_max`=85 WHERE  `mapId`=938 AND `difficulty`=1;
-UPDATE `world`.`access_requirement` SET `level_max`=85 WHERE  `mapId`=940 AND `difficulty`=0;
-UPDATE `world`.`access_requirement` SET `level_max`=85 WHERE  `mapId`=940 AND `difficulty`=1;
