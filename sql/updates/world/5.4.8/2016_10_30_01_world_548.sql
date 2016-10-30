a18e93575cfc5670800102313ff0174f46cf713a
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-30 15:45:30 -0300

    Core/Spell: Added Spell Dk Shadow Infusion

---
+++ sql/updates/world/5.4.8/2016_10_30_01_world_548.sql
@@ -0,0 +1,6 @@
+-- Some Fix
+-- By Bodeguero for DeathCore.
+
+DELETE FROM `spell_script_names` WHERE ScriptName='spell_dk_shadow_infusion';
+INSERT INTO `spell_script_names` VALUES ('91342', 'spell_dk_shadow_infusion');
+
