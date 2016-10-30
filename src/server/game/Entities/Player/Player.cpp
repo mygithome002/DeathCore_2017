d498984271debddd0aaeb5a01bf7a9979cece88a
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-30 18:44:16 -0300

    Some Fix

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -4846,7 +4846,7 @@ Corpse* Player::CreateCorpse()
     _corpseLocation.WorldRelocate(*this);
 	
     _uf = getCFSRace();
-    const uint8       = (uint8) (_uf);
+    uint8       = (uint8) (_uf);
 
     uint8 skin = GetByteValue(PLAYER_BYTES, PLAYER_BYTES_OFFSET_SKIN_ID);
     uint8 face = GetByteValue(PLAYER_BYTES, PLAYER_BYTES_OFFSET_FACE_ID);
