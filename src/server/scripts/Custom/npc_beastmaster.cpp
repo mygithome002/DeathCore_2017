fc9715197bfb2ea6e513248d69e3881922730112
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-11-16 19:35:33 -0300

    Fix Build

--- src/server/scripts/Custom/npc_beastmaster.cpp
+++ src/server/scripts/Custom/npc_beastmaster.cpp
@@ -129,10 +129,11 @@ public:
 		return true;
 	}
 
-	bool OnGossipSelect(Player *player, Creature * m_creature, uint32 sender, uint32 action)
-	{
-		player->PlayerTalkClass->ClearMenus();
-		switch (action)
+	bool OnGossipSelect(Player* player, Creature * m_creature, uint32 /*sender*/, uint32 actions)
+        {
+        ClearGossipMenuFor(player);
+
+		switch (actions)
 		{
 
 		case 100:
