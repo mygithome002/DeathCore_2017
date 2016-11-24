5427f21a6bd5d6cc11abbc02a667413944f70709
Riztazz <felianther15@gmail.com>
2016-11-25 00:31:10 +0100

    Core/Movement: Smooth movement
    Implement smooth movement for all waypoint pathing and escortai
    
    Signed-off-by: Bodeguero <joaocarlos.jc.souza@gmail.com>

--- src/server/game/Movement/MotionMaster.h
+++ src/server/game/Movement/MotionMaster.h
@@ -24,6 +24,7 @@
 #include "Object.h"
 #include "MoveSplineInitArgs.h"
 #include "SplineChain.h"
+#include "WaypointManager.h"
 
 class MovementGenerator;
 class Unit;
@@ -162,6 +163,7 @@ class TC_GAME_API MotionMaster
         void MoveTaxiFlight(uint32 path, uint32 pathnode);
         void MoveDistract(uint32 time);
         void MovePath(uint32 path_id, bool repeatable);
+        void MovePath(WaypointPath& path, bool repeatable);
         void MoveRotate(uint32 time, RotateDirection direction);
 
     private:
