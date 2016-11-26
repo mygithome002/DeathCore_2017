13b0567786def01c280428861a254ae9ab48a9d5
Aokromes <Aokromes@users.noreply.github.com>
2016-11-26 02:50:17 +0100

    Core/DungeonFinder: disable map only for LFG
    Core/DungeonFinder: disable map only from looking for dungeon
    
    Signed-off-by: Bodeguero <joaocarlos.jc.souza@gmail.com>

--- src/server/game/Conditions/DisableMgr.cpp
+++ src/server/game/Conditions/DisableMgr.cpp
@@ -41,7 +41,7 @@ namespace
 
     DisableMap m_DisableMap;
 
-    uint8 MAX_DISABLE_TYPES = 8;
+    uint8 MAX_DISABLE_TYPES = 9;
 }
 
 void LoadDisables()
@@ -117,6 +117,7 @@ void LoadDisables()
             case DISABLE_TYPE_QUEST:
                 break;
             case DISABLE_TYPE_MAP:
+            case DISABLE_TYPE_LFG_MAP:
             {
                 MapEntry const* mapEntry = sMapStore.LookupEntry(entry);
                 if (!mapEntry)
@@ -343,6 +344,7 @@ bool IsDisabledFor(DisableType type, uint32 entry, Unit const* unit, uint8 flags
             break;
         }
         case DISABLE_TYPE_MAP:
+        case DISABLE_TYPE_LFG_MAP:
             if (Player const* player = unit->ToPlayer())
             {
                 MapEntry const* mapEntry = sMapStore.LookupEntry(entry);
